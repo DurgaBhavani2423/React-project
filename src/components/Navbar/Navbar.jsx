dcd8c8d023c2cbf634f25e85aee05756b91e24fa
unknown <durgabhavanid83@gmail.com>
2024-04-07 14:57:13 +0530

    youtube

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import search_icon from '../../assets/search.png'
 import upload_icon from '../../assets/upload.png'
 import more_icon from '../../assets/more.png'
 import notification_icon from '../../assets/notification.png'
-import profile_icon from '../../assets/jack.png'
+import profile from '../../assets/profile.jpg'
 import { Link } from 'react-router-dom'
 
 
@@ -32,7 +32,7 @@ const Navbar = ({setsidebar}) => {
             <img src={upload_icon} alt="" />
             <img src={more_icon} alt="" />
             <img src={notification_icon} alt="" />
-            <img src={profile_icon} className='user-icon' alt="" />
+            <img src={profile} className='user-icon' alt="" />
         </div>
 
     </nav>
